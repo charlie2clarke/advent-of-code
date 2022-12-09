44ddf07016b1c547a8d870e3e1e7d1ce2c2cab7e
charlie2clarke <charliegclarke2@gmail.com>
2022-12-09 22:16:32 +0000

    day 9

--- 2022/09/9.go
+++ 2022/09/9.go
@@ -5,71 +5,93 @@ import (
 	"image"
 	"os"
 	"strings"
+
+	"github.com/charlie2clarke/advent-of-code/datastructures"
 )
+
+var deltas = map[string]image.Point{
+	"L":  {-1, 0},
+	"R":  {1, 0},
+	"U":  {0, 1},
+	"D":  {0, -1},
+	"UL": {-1, 1},
+	"DL": {-1, -1},
+	"UR": {1, 1},
+	"DR": {1, -1},
+}
 
 func main() {
 	file, _ := os.ReadFile("input.txt")
 	input := strings.Split(strings.TrimSpace(string(file)), "\n")
 
-	part1 := part1(input)
+	rope := [10]image.Point{}
 
-	fmt.Printf("Part 1: %v\n", part1)
-}
-
-func part1(input []string) int {
-	headPos, tailPos := image.Point{0, 0}, image.Point{0, 0}
-	gridVisited := map[image.Point]bool{
-		tailPos: true,
-	}
-
-	var (
-		LEFT  = image.Point{-1, 0}
-		RIGHT = image.Point{1, 0}
-		DOWN  = image.Point{0, -1}
-		UP    = image.Point{0, 1}
-	)
-
-	doMove := func(tailPos, delta image.Point, numToMove int) image.Point {
-		for newPos, i := tailPos.Add(delta), 1; i < numToMove; i++ {
-			newPos = newPos.Add(delta)
-			if i+1 == numToMove {
-				return newPos
-			}
-		}
-		return image.Point{}
-	}
-
-	isAdjacent := func(tailPos, headPos image.Point) bool {
-		for _, direction := range []image.Point{{-1, 0}, {1, 0}, {0, -1}, {0, 1}} {
-			if tailPos.Add(direction) == headPos {
-				return true
-			}
-		}
-		return false
-	}
+	gridVisitedPt1, gridVisitedPt2 := datastructures.NewSet[image.Point](), datastructures.NewSet[image.Point]()
+	gridVisitedPt1.Add(rope[0])
+	gridVisitedPt2.Add(rope[0])
 
 	for _, line := range input {
-		var direction string
-		var num int
+		var (
+			direction string
+			numToMove int
+		)
 
-		parses, err := fmt.Sscanf(line, "%s %d", &direction, &num)
-		if parses != 2 || err != nil {
-			panic("err parsing")
-		}
+		fmt.Sscanf(line, "%s %d", &direction, &numToMove)
 
-		switch direction {
-		case "R":
-			tailPos = doMove(tailPos, RIGHT, num)
-		case "L":
-			tailPos = doMove(tailPos, LEFT, num)
-		case "U":
-			tailPos = doMove(tailPos, UP, num)
-		case "D":
-			tailPos = doMove(tailPos, DOWN, num)
+		for i := 0; i < numToMove; i++ {
+			rope[0] = rope[0].Add(deltas[direction])
+
+			for knot := 1; knot < len(rope); knot++ {
+				rope[knot] = follow(rope[knot], rope[knot-1])
+			}
+
+			gridVisitedPt1.Add(rope[1])
+			gridVisitedPt2.Add(rope[len(rope)-1])
 		}
-		gridVisited[tailPos] = true
-		fmt.Println("oi")
 	}
 
-	return len(gridVisited)
+	fmt.Printf("Part 1: %v\n", gridVisitedPt1.Len())
+	fmt.Printf("Part 2: %v", gridVisitedPt2.Len())
+}
+
+func follow(tailPos, headPos image.Point) image.Point {
+	if headPos == tailPos {
+		return tailPos
+	}
+
+	if isOneSpaceAway(headPos, tailPos) {
+		return tailPos
+	}
+
+	diff := headPos.Sub(tailPos)
+	if abs(diff.X) > 0 || abs(diff.Y) > 0 {
+		tailPos = tailPos.Add(image.Point{sgn(diff.X), sgn(diff.Y)})
+	}
+
+	return tailPos
+}
+
+func isOneSpaceAway(headPos, tailPos image.Point) bool {
+	for _, delta := range deltas {
+		if tailPos.Add(delta) == headPos {
+			return true
+		}
+	}
+	return false
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func sgn(x int) int {
+	if x > 0 {
+		return 1
+	} else if x == 0 {
+		return 0
+	}
+	return -1
 }
